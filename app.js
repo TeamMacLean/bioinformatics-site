bb50aea80500f39ea5d50c469b9c6ee4ab8b3285
Martin Page <wookoouk@gmail.com>
2016-05-06 11:41:19 +0100

    example

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+var router = express.Router();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -9,6 +10,14 @@ app.set('view engine', 'ejs');
 
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+router.route('/')
+    .get(function (req, res) {
+        res.render('index');
+    });
+
+app.use(router);
 
 
 module.exports = app;
